900e7a1efdd9d37f97436f579de06737156ad45f
Sébastien Renoult <x.korel.x@gmail.com>
2018-11-24 16:59:20 +0100

    PROJET - Début Header et mise en place SCSS + fonts

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    /**
-     * Header principal du site
-     */
+    <title>DEV my Shirts | Développeurs de style</title>
 
-?>
+    <meta name="descripion" content="Boutique e-commerce en ligne de t-shirts originaux basés sur les langages de programmation et de développement">
+    <meta name="author" content="DEV my Shirts">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="">
+    
+    <!-- Liens vers les .CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/style/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/style/style.min.css" />
+</head>
+<body>
+
+<div class="container-fluid px-0">
+
+    <!-- //////////////////////// HEADER DE LA PAGE ////////////////////////// -->
+    <header>
+
+        <!-- //////////////////////// NAVBAR DU SITE ////////////////////////// -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-ocean">
+            <!-- Bouton Collapse -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent20"
+                aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="animated-icon1"><span></span><span></span><span></span></div>
+            </button>
+            <!-- Bouton Collapse -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Homme</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Femme</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nos Designers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Promo de Noël</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+</div>
