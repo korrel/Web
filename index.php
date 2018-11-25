85d0ded2721332d09e38367e68254cccac2c6345
Sébastien Renoult <x.korel.x@gmail.com>
2018-11-25 01:47:35 +0100

    PROJET - mise àjour du slide

--- index.php
+++ index.php
@@ -4,32 +4,49 @@ require_once(__DIR__.'/partials/header.php');
 ?>
 
         <!-- //////////////////////// SLIDE PRINCIPALE ////////////////////////// -->
+        <div class="container-fluid slide_container px-0">
+            
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active rounded-circle"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1" class="rounded-circle"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2" class="rounded-circle"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>...</h5>
+                        <p>...</p>
+                    </div>
+                <img class="d-block w-100" src="assets/image/slide01.jpg" alt="première slide">
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                            <h5>...</h5>
+                            <p>...</p>
+                        </div>
+                <img class="d-block w-100" src="assets/image/slide01.jpg" alt="seconde slide">
+                </div>
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>...</h5>
+                        <p>...</p>
+                    </div>
+                <img class="d-block w-100" src="assets/image/slide01.jpg" alt="troisième slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <div id="triangle-right"></div>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <div id="triangle-left"></div>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
 
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
         </div>
 
 
--- partials/header.php
+++ partials/header.php
@@ -33,43 +33,47 @@
                     DEV <span class="light-text">my Shirts</span>
                 </a>
                 <!-- Bouton Toggle -->
-                <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbar_links" aria-controls="navbar_links" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <!-- Liens -->
-                <div class="collapse navbar-collapse text-navbar justify-content-between" id="navbarNavAltMarkup">
-                    <!-- Menu principal -->
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link py-3 px-3" href="pages/gallery.php">Homme</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-3 px-3" href="pages/gallery.php">Femme</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-3 px-3" href="pages/designers.php">Nos Designers</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link promo py-3 px-3" href="pages/promo.php">Promo de Noël</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-3 px-3" href="#pages/contact.php">Contact</a>
-                        </li>
-                    </ul>
+                <div class="container mx">
+                         
+                    <div class="collapse navbar-collapse text-navbar justify-content-between" id="navbar_links">
+                        <!-- Menu principal -->
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link py-3 px-3" href="pages/gallery.php">Homme</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link py-3 px-3" href="pages/gallery.php">Femme</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link py-3 px-3" href="pages/designers.php">Nos Designers</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link promo py-3 px-3" href="pages/promo.php">Promo de Noël</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link py-3 px-3" href="#pages/contact.php">Contact</a>
+                            </li>
+                        </ul>
 
-                    <!-- Icones des réseaux sociaux -->
-                    <div class="row ml-3 my-2 my-lg-0 pb-2 pb-lg-0 ml-lg-0 mr-lg-3 gr_sociaux">
-                        <div class="col-1 col-lg-6 px-1">
-                            <a href="#">
-                                <img class="h-100" src="assets/image/facebook.svg" alt="Icone SVG Facebook">
-                            </a>
-                        </div>
-                        <div class="col-1 col-lg-6 px-1">
-                            <a href="#">
-                                <img class="h-100" src="assets/image/instagram.svg" alt="Icone SVG Instagram">
-                            </a>
+                        <!-- Icones des réseaux sociaux -->
+                        <div class="row ml-3 my-2 my-lg-0 pb-2 pb-lg-0 ml-lg-0 mr-lg-3 gr_sociaux">
+                            <div class="col-1 col-lg-6 px-1">
+                                <a href="#">
+                                    <img class="h-100" src="assets/image/facebook.svg" alt="Icone SVG Facebook">
+                                </a>
+                            </div>
+                            <div class="col-1 col-lg-6 px-1">
+                                <a href="#">
+                                    <img class="h-100" src="assets/image/instagram.svg" alt="Icone SVG Instagram">
+                                </a>
+                            </div>
                         </div>
                     </div>
+                    
                 </div>
             </nav>
 
