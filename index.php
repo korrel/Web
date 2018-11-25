5bd8a9cd19a528ddfdb34c5f593e8a7cd580703d
Sébastien Renoult <x.korel.x@gmail.com>
2018-11-25 14:56:07 +0100

    PROJET - Creation du slogan de la page d'index

--- index.php
+++ index.php
@@ -4,50 +4,66 @@ require_once(__DIR__.'/partials/header.php');
 ?>
 
         <!-- //////////////////////// SLIDE PRINCIPALE ////////////////////////// -->
-        <div class="container-fluid slide_container px-0">
+        <div class="container-fluid position-relative slide_container px-0">
             
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active rounded-circle"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1" class="rounded-circle"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2" class="rounded-circle"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>...</h5>
-                        <p>...</p>
-                    </div>
-                <img class="d-block w-100" src="assets/image/slide01.jpg" alt="première slide">
-                </div>
-                <div class="carousel-item">
-                    <div class="carousel-caption d-none d-md-block">
-                            <h5>...</h5>
-                            <p>...</p>
-                        </div>
-                <img class="d-block w-100" src="assets/image/slide01.jpg" alt="seconde slide">
-                </div>
-                <div class="carousel-item">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>...</h5>
-                        <p>...</p>
-                    </div>
-                <img class="d-block w-100" src="assets/image/slide01.jpg" alt="troisième slide">
-                </div>
+            <div class="container-fluid position-absolute h-100 w-100 shadow_content">
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <div id="triangle-right"></div>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <div id="triangle-left"></div>
-                <span class="sr-only">Next</span>
-            </a>
+
+            <div id="carouselcontroller" class="carousel slide" data-ride="carousel">
+                <!-- Les indicateurs de slide -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselcontroller" data-slide-to="0" class="active rounded-circle"></li>
+                    <li data-target="#carouselcontroller" data-slide-to="1" class="rounded-circle"></li>
+                    <li data-target="#carouselcontroller" data-slide-to="2" class="rounded-circle"></li>
+                </ol>
+                <!-- Les slides -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active d-flex justify-content-center">
+                        <div class="carousel-caption d-none d-md-block carousel_content">
+                            <p>Développe un style unique</p>
+                            <h5 class="slogan_slide">Porte un T-shirt Original</h5>
+                            <button type="button" class="btn btn-primary">Découvrir ></button>
+                        </div>
+                    <img class="d-block slide_visuel" src="assets/image/slide01.jpg" alt="première slide">
+                    </div>
+                </div>
+                <!-- Les controllers -->
+                <a class="carousel-control-prev" href="#carouselcontroller" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <div id="triangle-right"></div>
+                    <span class="sr-only">Précédent</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselcontroller" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <div id="triangle-left"></div>
+                    <span class="sr-only">Suivant</span>
+                </a>
             </div>
 
         </div>
+
+        <!-- //////////////////////// SLOGAN ////////////////////////// -->
+        <section class="container-fluid baseline_content">
+            <div class="container text-center py-5">
+                <h1 class="text-regular m-0">
+                    Les motifs de demain sont <span class="text-bleu">développés</span> aujourd'hui !
+                </h1>
+                <p>
+                    Démarquez-vous avec des t-shirts aux motifs innovants.
+                <p>
+            </div>
+        </section>
+
+        <!-- //////////////////////// GALLERIE NOUVEAUTES PRODUITS ////////////////////////// -->
+        <section class="container-fluid px-0">
+            <div class="container text-center py-5">
+                <h2>
+                    Les dernières nouveautés
+                </h2>
+            </div>
+        </section>
+
+        <!-- //////////////////////// SECTION IDEE MOTIF ////////////////////////// -->
 
 
 <?php
